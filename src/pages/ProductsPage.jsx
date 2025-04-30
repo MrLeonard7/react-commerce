53a68ed64de15c35b3570b9406f9b9a1de3ec553
DvLeo <dbecerra655@gmail.com>
2025-04-30 02:23:10 -0500

    agregar funcionalidad para cargar y mostrar productos en la página de productos

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,11 +1,51 @@
 // src/pages/ProductsPage.jsx
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import { getAllProducts } from '../services/productService'
+import { height } from '@fortawesome/free-brands-svg-icons/fa42Group'
+
 
 export const ProductsPage = () => {
+  const [products, setProducts] = useState([])
+
+  const loadProducts = async () => {
+    try {
+      setProducts(await getAllProducts())
+      console.log(products)
+    } catch (error) {
+      console.error("Error fetching products:", error.message)  
+    }
+  }
+
+  useEffect(() => {
+    loadProducts()
+  }, [])
+
   return (
-    <div>
-      <h2>Página de Productos</h2>
-      {/* Aquí mostraremos la lista/grid de productos más adelante */}
+    <div className="container-fluid">
+      <h2 className="my-4">Página de Productos</h2>
+      <p>Productos encontrados: {products.length}</p>
+      <div className="row">
+        {products.map((product) => (
+          <div key={product.id} 
+          className="col-auto mb-4"
+          style={{height: "319px", width: "206px"}}>
+            <div className="card h-100">
+              <img
+                src={product.image}
+                className="card-img-top img-fluid object-fit-scale align-self-center border rounded"
+                alt={product.title}
+                style={{ height: '130px', width: '132px', margin: '23px', }}
+              />
+              <div className="card-body pt-0">
+                <h5 className="card-title ">{product.title}</h5>
+                <p className="card-text">
+                  <strong>Precio: ${product.price}</strong>
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/services/productService.js
+++ src/services/productService.js
@@ -1,9 +1,11 @@
 export const getAllProducts = async () => {
     const url = "https://fakestoreapi.com/products"
     try {
-        await fetch(url)
+        const data = await fetch(url)
           .then((response) => response.json())
-          .then((data) => console.log(data))
+          .then((data) => data)
+
+        return data
         
     } catch (error) {
         console.error("Error fetching products:", error.message)
