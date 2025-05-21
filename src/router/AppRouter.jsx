2e6e44fec4d8697821a5c9916b16e4af24bf326b
Dv Leo <dbecerra655@gmail.com>
2025-05-21 14:58:07 -0500

    agregar componente Navbar y definir ruta para detalles del producto en AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,17 +4,17 @@ import { HomePage } from "../pages/HomePage"
 import { ProductsPage } from "../pages/ProductsPage"
 import { CartPage } from "../pages/CartPage"
 import { Navbar } from "../components/Navbar"
+import { ProductDetailPage } from "../pages/ProductDetailPage"
 
 export const AppRouter = () => {
     return (
         <> 
-            <Navbar />
             <Routes >
                 <Route path="/" element={<HomePage />}  />
                 <Route path="/products" element={<ProductsPage />} />
+                <Route path="/products/:productId" element={<ProductDetailPage/>} />
                 <Route path="/cart" element={<CartPage />} />
                 <Route path="*" element={<h2>404 Not Found</h2>} />
-                <Route path="/products/:productId"/>
             </Routes>
         </>
     )
